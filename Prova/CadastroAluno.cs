962e341aa2ce78f643c9c57c53ed08ddfa9f7cc9
Ricardo Prado Ribeiro <ricardodopradoribeiro@hotmail.com>
2022-08-30 15:26:07 -0300

    Adicionar arquivos de projeto.

---
+++ Prova/CadastroAluno.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prova
+{
+    internal class CadastroAluno
+    {
+
+        public string Nome;
+        public int Idade;
+        public string Escola;
+        public double Media;
+
+        public CadastroAluno()
+        {
+        }
+
+        public CadastroAluno(string nome, int idade, string escola)
+        {
+            Nome = nome;
+            Idade = idade;
+            Escola = escola;
+        }
+
+
+
+        public double Notas()
+        {
+
+            Console.WriteLine("Informe o nome do aluno ");
+            string nome = Console.ReadLine() ?? string.Empty;
+
+            Console.WriteLine("Informe a idade do aluno ");
+            int.TryParse(Console.ReadLine(), out int idade);
+
+            Console.WriteLine("Informe o nome da escola ");
+            string escola = Console.ReadLine() ?? string.Empty;
+            Nome = nome;
+            Idade = idade;
+            Escola = escola;
+
+            Console.WriteLine("________________________________");
+            Console.WriteLine("Agora informe as notas do aluno:");
+            Console.WriteLine("________________________________");
+
+            Console.WriteLine("Informe a nota do aluno em Matemática: ");
+            double notaMat = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Português: ");
+            double notaPort = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Ciências: ");
+            double notaCiencias = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Geografia: ");
+            double notaGeo = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em História: ");
+            double notaHist = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Educação Física: ");
+            double notaEdFisica = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Ensino Religioso: ");
+            double notaEnsReligioso = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Inglês: ");
+            double notaIngles = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("Informe a nota do aluno em Artes: ");
+            double notaArtes = double.Parse(Console.ReadLine() ?? "0");
+
+            Console.WriteLine("________________________________");
+
+            Media = ((notaMat + notaPort + notaCiencias + notaGeo + notaHist + notaEdFisica + notaEnsReligioso + notaIngles + notaArtes) / 9.0) / 10.0;
+
+            return Media;
+
+        }
+
+
+        
+
+
+
+
+
+    }
+}
---
+++ Prova/Program.cs
@@ -0,0 +1,144 @@
+﻿using Prova;
+using System;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+
+namespace ProgramaPrincipal
+{
+    class program
+    {
+        static List<CadastroAluno> AlunosCadastrados = new List<CadastroAluno>();
+        public static void Main(string[] args)
+        {
+
+            
+
+            int opcao2 = 1;
+            while (opcao2 == 1 || opcao2 == 2)
+            {
+                Console.WriteLine("---------Menu Principal---------");
+                Console.WriteLine("1. Cadastro Novo: ");
+                Console.WriteLine("2. Listas alunos: ");
+                Console.WriteLine("3. Sair");
+                
+                
+                var opcao = Console.ReadLine();
+                switch (opcao)
+                {
+                    case "1":
+                        var aluno = new CadastroAluno();
+                        aluno.Notas();
+                        AlunosCadastrados.Add(aluno);
+                        Console.WriteLine();
+                        break;
+                    case "2":
+                        ListarAlunos();
+                        Console.WriteLine();
+                        break;
+                    default:
+                            Console.WriteLine("Para sair, tecle Enter. ");
+                        int.TryParse(Console.ReadLine(), out opcao2);
+                        break;
+                            
+
+                            
+                    
+                        
+                        
+
+
+                }
+                
+            }
+
+        }
+
+
+
+        private static void ListarAlunos()
+        {
+            foreach(var aluno in AlunosCadastrados)
+            {
+                Console.WriteLine(aluno.Nome + " " + aluno.Idade + " " + aluno.Escola +" " + aluno.Media.ToString("F1"));
+                
+
+                if (aluno.Media >= 7.0)
+                {
+                    Console.WriteLine("Aprovado");
+                }
+                else if(aluno.Media > 2.0)
+                {
+                    Console.WriteLine("Recuperação");
+                }
+                else
+                {
+                    Console.WriteLine("Reprovado");
+                }
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+          
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
